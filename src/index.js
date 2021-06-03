32c24f2c040b702d8d6b11de21251dafd3c43572
rhaynes3k <rhaynes3k@gmail.com>
2021-06-03 03:13:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener('DOMContentLoaded', function () {
+  imgs();
+  dogPix();
+});
+let dogs = document.getElementById('dog-image-container')
+let h2 = document.createElement('h2')
+
+function dogPix(){
+  fetch(imgUrl)
+  .then(response => (response.json()))
+  .then(results => {
+      results.message.forEach(image => imgs(image))
+  })
+}
+
+function imgs(img){
+    let dogs = document.getElementById('dog-image-container')
+    let h2 = document.createElement('h2')
+    h2.src = img
+    dogs.appendChild(h2)
+}
