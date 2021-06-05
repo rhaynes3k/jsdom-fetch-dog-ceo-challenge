7eeff5aca8765bb5d9075573c510df175e845d9b
rhaynes3k <rhaynes3k@gmail.com>
2021-06-05 17:41:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
+
+
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-document.addEventListener('DOMContentLoaded', function () {
-  imgs();
-  dogPix();
-});
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 let dogs = document.getElementById('dog-image-container')
 let h2 = document.createElement('h2')
 
 function dogPix(){
   fetch(imgUrl)
-  .then(response => (response.json()))
-  .then(results => {
-      results.message.forEach(image => imgs(image))
+  .then(res => res.json())
+  .then(data => {
+      data.message.forEach(image => imgs(image))
+      // ?=console.log(data.message)
   })
 }
+// dogPix()
 
-function imgs(img){
+function imgs(image){
     let dogs = document.getElementById('dog-image-container')
-    let h2 = document.createElement('h2')
-    h2.src = img
+    let h2 = document.createElement('img')
+    h2.src = image
     dogs.appendChild(h2)
 }
+
+function dogBreeds(){
+  fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(data => {
+    // console.log(data.message)
+    for(const breed in data.message){
+      breedLi(breed)
+      // console.log(`${breed}: ${data.message[breed]}`)
+    }
+  })
+}
+dogBreeds()
+
+function breedLi(breed){
+  let breedList = document.getElementById('dog-breeds')
+  let brdLst = document.createElement('li')
+  brdLst.innerHTML = breed
+  console.log(breed)
+  breedList.appendChild(brdLst)
+}
+document.addEventListener('DOMContentLoaded', function () {
+  imgs()
+  dogPix()
+  dogBreeds()
+  breedLi()
+})
